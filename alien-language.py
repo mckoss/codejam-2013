6a17af8f40be26dc2dfaec0e50860e1c5c33077f
Mike Koss <mckoss@startpad.org>
2013-04-12 16:54:34 -0700

    faster

--- alien-language.py
+++ alien-language.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # https://code.google.com/codejam/contest/32016/dashboard#s=p0
+import re
 import fileinput
 
 test_input = """\
@@ -20,15 +21,15 @@ def parse(lines):
     """
     >>> words, tests = parse(iter(test_input))
     >>> words
-    set(['dbc', 'abc', 'cba', 'bca', 'dac'])
+    ['abc', 'bca', 'dac', 'dbc', 'cba']
     >>> tests
     ['(ab)(bc)(ca)', 'abc', '(abc)(abc)(abc)', '(zyx)bc']
     """
     nums = lines.next().split()
     _l, d, n = [int(x) for x in nums]
-    words = set()
+    words = []
     for _i in range(d):
-        words.add(lines.next())
+        words.append(lines.next())
     tests = []
     for _i in range(n):
         tests.append(lines.next())
@@ -44,42 +45,31 @@ def main(lines):
     Case #4: 0
     """
     words, tests = parse(lines)
-    for i, pattern in enumerate(tests):
+    patterns = [re.compile(pattern_to_regex(test)) for test in tests]
+    for i, pattern in enumerate(patterns):
         feasible = 0
-        for word in possible_words(pattern):
-            if word in words:
+        for word in words:
+            if pattern.match(word):
                 feasible += 1
         print "Case #%d: %d" % (i + 1, feasible)
 
 
-def possible_words(pattern):
+def pattern_to_regex(test):
     """
-    >>> list(possible_words('ab'))
-    ['ab']
-    >>> list(possible_words('(ab)c'))
-    ['ac', 'bc']
-    >>> list(possible_words('a(bc)'))
-    ['ab', 'ac']
-    >>> list(possible_words('(a)(bc)'))
-    ['ab', 'ac']
+    >>> pattern_to_regex('ab')
+    'ab'
+    >>> pattern_to_regex('(ab)c')
+    '(a|b)c'
+    >>> pattern_to_regex('a(bc)')
+    'a(b|c)'
     """
-    if pattern == '':
-        yield ''
-        return
-    if pattern[0] != '(':
-        letters = [pattern[0]]
-        pattern = pattern[1:]
-    else:
-        close_paren = pattern.index(')')
-        letters = pattern[1:close_paren]
-        pattern = pattern[close_paren + 1:]
-    for suffix in possible_words(pattern):
-        for letter in letters:
-            yield letter + suffix
-
-
-def dot_product(v1, v2):
-    return sum([p * q for p, q in zip(v1, v2)])
+    if test == '':
+        return ''
+    if test[0] != '(':
+        return test[0] + pattern_to_regex(test[1:])
+    close_paren = test.index(')')
+    or_exp = '|'.join(list(test[1:close_paren]))
+    return "(%s)%s" % (or_exp, pattern_to_regex(test[close_paren + 1:]))
 
 
 if __name__ == '__main__':
