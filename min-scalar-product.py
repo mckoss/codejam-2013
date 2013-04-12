2f3019a91cc81b2561c5442e4394dfa26d50fd3f
Mike Koss <mckoss@startpad.org>
2013-04-12 15:12:44 -0700

    use fileinput module

--- min-scalar-product.py
+++ min-scalar-product.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python
-from itertools import permutations
+# https://code.google.com/codejam/contest/32016/dashboard#s=p0
+import fileinput
 
-def main():
-    with open('test.txt') as input:
-        cases = int(input.readline())
-        for case_number in range(1, cases+1):
-            input.readline()
-            v1 = [int(x) for x in input.readline().split()]
-            v1.sort()
-            v2 = [int(x) for x in input.readline().split()]
-            v2.sort()
-            v2.reverse()
-            print "Case #%d: %d" % (case_number, dot_product(v1, v2))
+
+def main(lines):
+    cases = int(lines.next())
+    for case_number in range(1, cases + 1):
+        lines.next()
+        v1 = [int(x) for x in lines.next().split()]
+        v1.sort()
+        v2 = [int(x) for x in lines.next().split()]
+        v2.sort()
+        v2.reverse()
+        print "Case #%d: %d" % (case_number, dot_product(v1, v2))
 
 
 def dot_product(v1, v2):
     return sum([p * q for p, q in zip(v1, v2)])
 
+
 if __name__ == '__main__':
-    main()
+    main(fileinput.input())
