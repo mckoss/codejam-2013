93fb41ceae0bfd3e51a19d1b7b6a231737c22afd
Mike Koss <mckoss@startpad.org>
2013-04-13 01:51:15 -0700

    palindrome helper

--- fair-and-square.py
+++ fair-and-square.py
@@ -38,5 +38,84 @@ def main(lines):
         print "Case #%d: %d" % (i + 1, n)
 
 
+def next_palindrome(x):
+    """
+    >>> next_palindrome(0)
+    1
+    >>> next_palindrome(1)
+    2
+    >>> next_palindrome(9)
+    11
+    >>> next_palindrome(11)
+    22
+    >>> next_palindrome(100)
+    101
+    >>> next_palindrome(101)
+    111
+    >>> next_palindrome(111)
+    121
+    >>> next_palindrome(290)
+    292
+    >>> next_palindrome(205)
+    212
+    >>> next_palindrome(295)
+    303
+    """
+    s = str(x)
+    is_odd = len(s) % 2 == 1
+    half_len = len(s) // 2
+    l = s[:half_len]
+    if is_odd:
+        m = s[half_len]
+        r = s[:half_len:-1]
+    else:
+        m = ''
+        r = s[:half_len - 1:-1]
+    if l > r:
+        return int(l + m + l[::-1])
+    if is_odd:
+        if m != '9':
+            m = str(int(m) + 1)
+            return int(l + m + l[::-1])
+        m = '0'
+        if half_len == 0:
+            l = '0'
+        l = str(int(l) + 1)
+        if len(l) > half_len:
+            return int(l + l[::-1])
+        return int(l + m + l[::-1])
+    l = str(int(l) + 1)
+    if len(l) > half_len:
+        return int(l + l[:1:-1])
+    return int(l + l[::-1])
+
+
+def isqrt(x):
+    """
+    >>> isqrt(1)
+    1
+    >>> isqrt(4)
+    2
+    >>> isqrt(10**6)
+    1000
+    >>> isqrt(11111111 * 11111111)
+    11111111L
+    >>> isqrt(12345678901234567890L * 12345678901234567890L)
+    12345678901234567890L
+    >>> isqrt(33)
+    5
+    """
+    a = 1
+    b = x // 2
+    if x == 1:
+        return 1
+    while True:
+        a = b
+        b = (a + x // a) // 2
+        if abs(b - a) <= 1:
+            break
+    return min(a, b)
+
+
 if __name__ == '__main__':
     main(fileinput.input())
